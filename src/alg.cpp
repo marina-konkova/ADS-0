35790fe29b52c01c4702f5b93e2e351da5438b41
marina-konkova <116936403+marina-konkova@users.noreply.github.com>
2023-02-19 13:10:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,13 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    if (a == b) {
+        return b;
+    } else {
+        if (a < b) {
+            return gcd(a, b - a);
+        } else {
+            return gcd(a - b, b);
+        }
+    }
 }
